60ce57086a2bd232190a0f09cb934d00392b7eb9
Astinode Technologies <astinodetechnologies@gmail.com>
2026-08-19 10:03:09 +0530

    fix: import styles.css directly in __root.tsx so CSS stylesheets are bundled and loaded

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,6 +11,7 @@ import { useEffect, type ReactNode } from "react";
 
 import { Toaster } from "@/components/ui/sonner";
 
+import "../styles.css";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
